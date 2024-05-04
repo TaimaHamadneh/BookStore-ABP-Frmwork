fce31c5092d50ff35fb2f11f4dbb9a655f51e70d
TaimaHamadneh <taimanizar45@gmail.com>
2024-05-04 04:15:12 +0300

    Book Store ABP Framwork

---
+++ aspnet-core/test/Acme.BookStore.MongoDB.Tests/MongoDb/Applications/Authors/MongoDBAuthorAppService_Tests.cs
@@ -0,0 +1,11 @@
+﻿using Acme.BookStore.MongoDB;
+using Acme.BookStore.Authors;
+using Xunit;
+
+namespace Acme.BookStore.MongoDb.Applications.Authors;
+
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class MongoDBAuthorAppService_Tests : AuthorAppService_Tests<BookStoreMongoDbTestModule>
+{
+
+}
---
+++ aspnet-core/test/Acme.BookStore.MongoDB.Tests/MongoDb/Applications/Books/MongoDBBookAppService_Tests.cs
@@ -0,0 +1,12 @@
+﻿using Acme.BookStore.MongoDB;
+using Acme.BookStore.Books;
+using Xunit;
+
+namespace Acme.BookStore.MongoDb.Applications.Books;
+
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class MongoDBBookAppService_Tests : BookAppService_Tests<BookStoreMongoDbTestModule>
+{
+
+
+}
